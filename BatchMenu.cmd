d72f1bf32c9453b42d661f6281f193ee1381d3c5
EmuZoneDEV <52322805+EmuZoneDEV@users.noreply.github.com>
2019-07-22 01:08:09 +0200

    Create BatchMenu.cmd

---
+++ BatchMenu.cmd
@@ -0,0 +1,96 @@
+@ECHO OFF
+C:
+CD\
+CLS
+
+:MENU
+CLS
+
+ECHO ============= MENU NAME =============
+ECHO -------------------------------------
+ECHO 1.  Selection 1
+ECHO 2.  Selection 2
+ECHO 3.  Selection 3
+ECHO 4.  Selection 4
+ECHO 5.  Selection 5
+ECHO 6.  Selection 6
+ECHO 7.  Selection 7
+ECHO -------------------------------------
+ECHO 8.  Selection 8
+ECHO -------------------------------------
+ECHO 9.  Selection 9
+ECHO -------------------------------------
+ECHO ==========PRESS 'Q' TO QUIT==========
+ECHO.
+
+SET INPUT=
+SET /P INPUT=Please select a number:
+
+IF /I '%INPUT%'=='1' GOTO Selection1
+IF /I '%INPUT%'=='2' GOTO Selection2
+IF /I '%INPUT%'=='3' GOTO Selection3
+IF /I '%INPUT%'=='4' GOTO Selection4
+IF /I '%INPUT%'=='5' GOTO Selection5
+IF /I '%INPUT%'=='6' GOTO Selection6
+IF /I '%INPUT%'=='7' GOTO Selection7
+IF /I '%INPUT%'=='8' GOTO Selection8
+IF /I '%INPUT%'=='9' GOTO Selection9
+IF /I '%INPUT%'=='Q' GOTO Quit
+
+CLS
+
+ECHO ============INVALID INPUT============
+ECHO -------------------------------------
+ECHO Please select a number from the Main
+echo Menu [1-9] or select 'Q' to quit.
+ECHO -------------------------------------
+ECHO ======PRESS ANY KEY TO CONTINUE======
+
+PAUSE > NUL
+GOTO MENU
+
+:Selection1
+
+Whatever code you want goes here...
+
+:Selection2
+
+and in here...
+
+:Selection3
+
+and in here too...
+
+:Selection4
+
+and so on
+
+:Selection5
+
+and so on
+
+:Selection6
+
+and so on
+
+:Selection7
+
+and so on
+
+:Selection8
+
+and so on
+
+:Selection9
+
+and so on
+
+:Quit
+CLS
+
+ECHO ==============THANKYOU===============
+ECHO -------------------------------------
+ECHO ======PRESS ANY KEY TO CONTINUE======
+
+PAUSE>NUL
+EXIT
